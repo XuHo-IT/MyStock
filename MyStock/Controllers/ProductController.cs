ee25123323197b18d6c123181d327ff3de71acca
XuHo-IT <ngotranxuanhoa09062004@gmail.com>
2025-10-03 14:26:08 +0700

    first commit

---
+++ MyStock/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyStock.ModelsData;
+
+namespace MyStock.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MyStockContext context;
+
+        public ProductController(MyStockContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = context.Products.ToList();
+            return View(model);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = context.Products.FirstOrDefault(m => m.Productid == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(product);
+                context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Product product)
+        {
+            if (id != product.Productid)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                context.Update(product);
+                context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product != null)
+            {
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MyStock/ModelsData/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyStock.ModelsData;
+
+public partial class Product
+{
+    public int Productid { get; set; }
+
+    public string Productname { get; set; } = null!;
+
+    public decimal Unitprice { get; set; }
+
+    public int Unitsinstock { get; set; }
+}
